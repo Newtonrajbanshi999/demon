c3b8ee1947902b26f1f0f644cb4df73185a2b965
newtonrajbanshi999 <newtonrajbanshi99@gmail.com>
2022-08-10 12:06:29 +0545

    commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Kindacode.com',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Demon'),
+        actions: [
+          IconButton(
+              onPressed: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => const SearchPage())),
+              icon: const Icon(Icons.search))
+        ],
+      ),
+      body: Container(
+        height: 50,
+        width: 250,
+        decoration: BoxDecoration(
+            color: Colors.indigo, borderRadius: BorderRadius.circular(20)),
+        child: FlatButton(
+          onPressed: () {},
+          child: Text(
+            'Get started',
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController inputTextController =
+        new TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+          title: Container(
+        width: double.infinity,
+        height: 40,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(5)),
+        child: Center(
+          child: TextField(
+            controller: inputTextController,
+            decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () => inputTextController.clear(),
+                ),
+                hintText: 'Search...',
+                border: InputBorder.none),
+          ),
+        ),
+      )),
+    );
+  }
+}
